5455e90d08c34f4f1b13f5dbfaa32535a7d8fdec
Renato Delboni <renatonato_jr@hotmail.com>
2021-12-27 16:40:44 +0900

    JS setup (getting html elemetns)

---
+++ app.js
@@ -0,0 +1,15 @@
+const alert = document.querySelector('.alert');
+const form = document.querySelector('.grocery-form');
+const submitButton = document.querySelector('.submitButton');
+const clearButton = document.querySelector('.clearButton');
+const container = document.querySelector('.grocery-container');
+const list = document.querySelector('.grocery-list');
+const grocery = document.getElementById('.grocery');
+
+// Variables
+
+let editElement;
+let editFlag = false;
+let editID = "";
+
+
