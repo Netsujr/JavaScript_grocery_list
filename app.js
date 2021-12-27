6c463bb519f0e559fb4a968e82c0aef2a7170a58
Renato Delboni <renatonato_jr@hotmail.com>
2021-12-27 17:29:04 +0900

    adding an item works. imported FA
    forgot font-Awesome, import link at the start of project, pay attention next time so you wont spend time debugging 'why the icons aren't appearing...'

--- app.js
+++ app.js
@@ -23,12 +23,34 @@ function addItem(event) {
   const value = grocery.value;
   const id = new Date().getTime().toString(); // little cheat to creat unique ids
   // console.log(id);
-  if(value && !editFlag) {
+  if (value && !editFlag) {
     // console.log("Add item");
-  } else if(!value && editFlag) {
+    const element = document.createElement('article');
+    element.classList.add('grocery-item'); // adding a class
+    let attribute = document.createAttribute('data-id'); // adding id
+    attribute.value = id;
+    element.setAttributeNode(attribute);
+    element.innerHTML = `
+    <p class="title">${value}</p>
+      <div class="btn-container">
+        <button type="button" class="edit-button">
+          <i class="fas fa-edit"></i>
+        </button>
+        <button type="button" class="delete-button">
+          <i class="fas fa-trash"></i>
+        </button>
+      </div>
+    `;
+      // adding  to list
+      list.appendChild(element);
+      displayAlert('Item Added to List', 'success');
+      container.classList.add('show-container');
+
+
+  } else if (!value && editFlag) {
     // console.log("Edit Item");
   } else {
-    // console.log("Display Alert");
+    console.log("Display Alert");
     // alert.textContent = 'No Groceries Entered';
     // alert.classList.add('alert-danger');
     displayAlert('No groceries Entered', 'danger');
@@ -42,8 +64,8 @@ function displayAlert(text, action) {
 
   setTimeout(function () {
     alert.textContent = '';
-    // alert.classList.remove(`alert-${action}`);
-    alert.style.opacity = '0';
+    alert.classList.remove(`alert-${action}`);
+    // alert.style.visibility = 'hidden';
   }, 1500);
 }
 // ------------
