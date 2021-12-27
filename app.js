de7587c55145e20d607995a0fd2ea3d35acffc0b
Renato Delboni <renatonato_jr@hotmail.com>
2021-12-27 16:54:33 +0900

    basic functionality work. Fixed grocery const

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const submitButton = document.querySelector('.submitButton');
 const clearButton = document.querySelector('.clearButton');
 const container = document.querySelector('.grocery-container');
 const list = document.querySelector('.grocery-list');
-const grocery = document.getElementById('.grocery');
+const grocery = document.getElementById('grocery');
 
 // Variables
 let editElement;
@@ -21,6 +21,12 @@ function addItem(event) {
   const value = grocery.value;
   const id = new Date().getTime().toString(); // little cheat to creat unique ids
   // console.log(id);
-  
+  if(value && editFlag === false) {
+    console.log("Add item");
+  } else if(!value && editFlag === true) {
+    console.log("Edit Item");
+  } else {
+    console.log("Nothing to see here");
+  }
 }
 // ------------
